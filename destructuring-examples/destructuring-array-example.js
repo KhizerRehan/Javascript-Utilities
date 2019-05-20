45de264e15bee6e8c1798094201b077286eaefa1
khizerrehan <khizerrehan92@gmail.com>
2019-05-21 01:51:23 +0500

    destructing: add array destructing example

---
+++ destructuring-examples/destructuring-array-example.js
@@ -0,0 +1,12 @@
+// Example-1
+const [first,second,third] = ['first-val','second-val','third-val'];
+console.log('First Value:',first);
+console.log('Second Value:',second);
+console.log('Third Value:',third);
+
+// Example-2
+var things = ["Table", "Chair", "Fan", "Rug"];
+var [a, b, c, d, e] = things;
+console.log(c); //Output: Fan
+console.log(d); //Output: Rug
+console.log(e); //Output: undefined
